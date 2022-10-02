209fe76075cf03e2af8e2aad49b44379d7dbc511
nn_truong1106 <truong11062002@gmail.com>
2022-10-02 14:08:13 +0700

    fix: fix ChooseTopic
    fix: fix ChooseTopic

--- Assignments/TH01/Game_mini/Game_mini/ChooseTopic.Designer.cs
+++ Assignments/TH01/Game_mini/Game_mini/ChooseTopic.Designer.cs
@@ -33,19 +33,19 @@ namespace Game_mini
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btn_Vehicle = new System.Windows.Forms.Button();
+            this.btn_Fruit = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -84,108 +84,112 @@ namespace Game_mini
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pic_Click_Back);
             // 
-            // groupBox1
+            // panel2
             // 
-            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.button4);
-            this.groupBox1.Controls.Add(this.button3);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(7, 81);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(962, 506);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
-            // 
-            // button1
-            // 
-            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
-            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button1.Location = new System.Drawing.Point(85, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(299, 166);
-            this.button1.TabIndex = 0;
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.Location = new System.Drawing.Point(521, 21);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(299, 166);
-            this.button2.TabIndex = 4;
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
-            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button3.Location = new System.Drawing.Point(85, 265);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(299, 166);
-            this.button3.TabIndex = 5;
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
-            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button4.Location = new System.Drawing.Point(521, 265);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(299, 166);
-            this.button4.TabIndex = 6;
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Brown;
-            this.label1.Location = new System.Drawing.Point(169, 204);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(115, 33);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "FRUIT";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Brown;
-            this.label2.Location = new System.Drawing.Point(593, 204);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(158, 33);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "VEHICLE";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Brown;
-            this.label3.Location = new System.Drawing.Point(169, 443);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(140, 33);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "ANIMAL";
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.btn_Vehicle);
+            this.panel2.Controls.Add(this.btn_Fruit);
+            this.panel2.Location = new System.Drawing.Point(7, 84);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(961, 501);
+            this.panel2.TabIndex = 7;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Brown;
-            this.label4.Location = new System.Drawing.Point(566, 434);
+            this.label4.Location = new System.Drawing.Point(594, 436);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(226, 33);
-            this.label4.TabIndex = 10;
+            this.label4.TabIndex = 18;
             this.label4.Text = "OCCUPATION";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Brown;
+            this.label3.Location = new System.Drawing.Point(197, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 33);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "ANIMAL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Brown;
+            this.label2.Location = new System.Drawing.Point(621, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 33);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "VEHICLE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Imprint MT Shadow", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(197, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 33);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "FRUIT";
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(549, 267);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(299, 166);
+            this.button4.TabIndex = 14;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_Click_Occupation);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(113, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(299, 166);
+            this.button3.TabIndex = 13;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_Click_Animal);
+            // 
+            // btn_Vehicle
+            // 
+            this.btn_Vehicle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Vehicle.BackgroundImage")));
+            this.btn_Vehicle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Vehicle.Location = new System.Drawing.Point(549, 23);
+            this.btn_Vehicle.Name = "btn_Vehicle";
+            this.btn_Vehicle.Size = new System.Drawing.Size(299, 166);
+            this.btn_Vehicle.TabIndex = 12;
+            this.btn_Vehicle.UseVisualStyleBackColor = true;
+            this.btn_Vehicle.Click += new System.EventHandler(this.btn_Click_Vehicle);
+            // 
+            // btn_Fruit
+            // 
+            this.btn_Fruit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Fruit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Fruit.BackgroundImage")));
+            this.btn_Fruit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Fruit.Location = new System.Drawing.Point(113, 23);
+            this.btn_Fruit.Name = "btn_Fruit";
+            this.btn_Fruit.Size = new System.Drawing.Size(299, 166);
+            this.btn_Fruit.TabIndex = 11;
+            this.btn_Fruit.UseVisualStyleBackColor = false;
+            this.btn_Fruit.Click += new System.EventHandler(this.btn_Click_Fruit);
+            this.btn_Fruit.MouseLeave += new System.EventHandler(this.shipBtn_Fruit_MouseLeave);
+            this.btn_Fruit.MouseHover += new System.EventHandler(this.shipBtn_Fruit_MouseHover);
             // 
             // ChooseTopic
             // 
@@ -194,7 +198,7 @@ namespace Game_mini
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(973, 599);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ChooseTopic";
@@ -203,8 +207,8 @@ namespace Game_mini
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -214,14 +218,14 @@ namespace Game_mini
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Vehicle;
+        private System.Windows.Forms.Button btn_Fruit;
     }
 }
--- Assignments/TH01/Game_mini/Game_mini/ChooseTopic.cs
+++ Assignments/TH01/Game_mini/Game_mini/ChooseTopic.cs
@@ -1,4 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Game_mini
@@ -28,6 +35,50 @@ namespace Game_mini
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btn_Click_Fruit(object sender, EventArgs e)
+        {
+            Fruit f_Fruit = new Fruit();
+            f_Fruit.Show(this);
+        }
+
+        private void btn_Click_Vehicle(object sender, EventArgs e)
+        {
+            Vehicle f_Vehicle = new Vehicle();
+            f_Vehicle.Show(this);
+        }
+
+        private void btn_Click_Animal(object sender, EventArgs e)
+        {
+            Animal f_Animal = new Animal();
+            f_Animal.Show(this);
+        }
+
+        private void btn_Click_Occupation(object sender, EventArgs e)
+        {
+            Occupation f_Occupation = new Occupation();
+            f_Occupation.Show(this);
+        }
+
+        private void shipBtn_Fruit_MouseHover(object sender, EventArgs e)
+        {
+            btn_Fruit.BackColor = Color.LightSkyBlue;
+        }
+
+        private void shipBtn_Fruit_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Fruit.BackColor = Color.Transparent;
+        }
+
+        private void shipBtn_Vehicle_MouseHover(object sender, EventArgs e)
+        {
+            btn_Vehicle.BackColor = Color.LightSkyBlue;
+        }
+
+        private void shipBtn_Vehicle_MouseLeave(object sender, EventArgs e)
+        {
+            btn_Vehicle.BackColor = Color.Transparent;
         }
     }
 }
