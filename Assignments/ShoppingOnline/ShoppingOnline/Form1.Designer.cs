bc8ecde2242343b9a1abea89938becbfa418a4be
nn_truong1106 <truong11062002@gmail.com>
2022-10-31 13:11:43 +0700

    dev: init project

--- Assignments/ShoppingOnline/ShoppingOnline/Form1.Designer.cs
+++ Assignments/ShoppingOnline/ShoppingOnline/Form1.Designer.cs
@@ -29,10 +29,17 @@ namespace ShoppingOnline
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(732, 402);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
