fc292f835bbaa169dfcd6d830521eb6c5114604d
nn_truong1106 <truong11062002@gmail.com>
2022-11-19 13:41:06 +0700

    DONE

--- Assignments/ShoppingOnline/ShoppingOnline/main.Designer.cs
+++ Assignments/ShoppingOnline/ShoppingOnline/main.Designer.cs
@@ -204,7 +204,7 @@ namespace ShoppingOnline
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(519, 36);
+            this.label6.Location = new System.Drawing.Point(525, 36);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(56, 25);
             this.label6.TabIndex = 3;
